65d60533b551c89d274966f4c975b9484ae5c74d
1cfgs <1cfgs@DESKTOP-30E9MJG>
2021-03-03 11:46:32 +0000

    paso 1 y 2

---
+++ src/main/java/com/mycompany/calculadora_final/operador2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calculadora_final;
+
+/**
+ *
+ * @author 1cfgs
+ */
+public class operador2 {
+    
+}
